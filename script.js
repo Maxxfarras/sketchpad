adb8affc209ddf29f7f065109e5b33a7b71047a8
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2024-06-06 12:13:45 -0700

    return to last commit

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const container = document.querySelector('#container')
 
-let rowNum = prompt('how many rows?');
-
-function makeRows (rowNum) {
-    for (i = 0; i < rowNum; i++){
-        let rows = document.createElement('div');
-        container.appendChild(rows);
+let input = prompt('how many rows?');
+/*
+function makeGrid (input) {
+    for (rows = 0; rows < input; rows++) {
+        for (column = 0; column < input; column++){
+            container.append("<div class='grid'></div>");
+        };
     };
+    $('.grid').width(960/input);
+    $('.grid').height(960/input);
+};*/
+//makeGrid(input);
+
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").width(960/x);
+    $(".grid").height(960/x);
 };
 
-makeRows(rowNum);
+createGrid(16)
